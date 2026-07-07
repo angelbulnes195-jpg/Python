1d98572e53371f667f8fae236ae262417e6bec85
Angel Bulnes <angelbulnes195@gmail.com>
2026-07-07 10:47:38 -0600

    Add practice files into src directory

---
+++ src/data type/simple-data-type.py
@@ -0,0 +1,21 @@
+# str
+
+"string"
+'string'
+
+"""my data is:
+name: Angel Bulnes
+age: 14"""
+
+'''my data is:
+name: Angel Bulnes
+age: 14''' 
+
+#int
+202
+
+#float
+3.14159
+
+#boolean
+True    
---
+++ src/data type/structured-data-type.py
@@ -0,0 +1,22 @@
+# list
+list = ["Angel", "Bulnes", 2026, True, 3.1415926535]
+# list[0] = "Alice"
+print(list[0])
+
+# tuple
+tuple = ("Angel", "Bulnes", 2026, True, 3.1415926535)
+print(tuple[4])
+
+# set
+set = {"Angel", "Bulnes", 2026, True, 3.1415926535}
+print(set)
+
+# dict -- structure = 'key': value,
+dict = {
+    'name': "Angel",
+    'last_name': "Bulnes",
+    'year': 2026,
+    'is_student': True,
+    'pi': 3.1415926535
+}
+print(dict["is_student"])
