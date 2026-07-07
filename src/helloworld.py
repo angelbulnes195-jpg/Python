ac294d70221373d8b9f86e36e529818df337de1b
Angel Bulnes <angelbulnes195@gmail.com>
2026-07-07 10:17:56 -0600

    simple and structured data type and variables

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,2 +1,3 @@
 print("hello world")
 print(2 + 2)
+print("Angel Bulnes 2026")
---
+++ variables/variables.py
@@ -0,0 +1,23 @@
+a = 659
+b = 49
+c = a + b
+
+print(c)
+
+# Defining variables
+name = "Angel B."
+name = "Angel Bulnes"
+
+# concatenar con +
+welcome_Angel = "Hello, " + name + " welcome to Python programming!"
+
+
+print(welcome)
+
+name = 3.14159265358979323
+
+# concatenar con f-strings
+welcome =f"Hello, {name} welcome to Python programming!"
+
+print("Hello" not in welcome) #false
+print("Hello" in welcome) #true
