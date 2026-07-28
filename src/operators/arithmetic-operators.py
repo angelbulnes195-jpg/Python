02808a9a346deee32ae3c24ea01482220f26ac4b
Angel Bulnes <angelbulnes195@gmail.com>
2026-07-28 10:01:24 -0600

    Angel Bulnes Python Portfolio

---
+++ src/operators/arithmetic-operators.py
@@ -0,0 +1,19 @@
+# addition and substraction ( + and - )
+addition = 12 + 6
+subtraction = 12 - 6
+
+# multiplication and division ( * and / )
+multiplication = 12 * 6
+division = 12 / 6 #gives float value
+
+# exponentiation ( ** )
+exponentiation = 12 ** 6
+
+# low division ( // )
+low_division = 12 // 6 #gives int value
+
+# modulus ( % )
+mod = 12 % 6
+
+print(exponentiation)
+print(division)
---
+++ src/operators/comparison-operators.py
@@ -0,0 +1,36 @@
+is_equal_to = 5 == 6
+
+is_diferent_from = 5 != 6
+
+is_greater_than = 5 > 6
+
+is_less_than = 5 < 6
+
+is_greater_than_or_equal_to = 5 >= 6
+
+is_less_than_or_equal_to = 5 <= 6
+
+print(is_greater_than_or_equal_to)
+
+
+
+# combined calculations
+
+a = 5
+b = 10
+c =23
+
+comparison = a + b < c
+print(comparison)
+
+
+
+# compare users
+
+database_user = "Angel F Bulnes"
+user_written = "Angelin"
+
+print(database_user == user_written)
+
+
+
