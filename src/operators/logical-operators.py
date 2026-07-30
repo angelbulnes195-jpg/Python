044436594dd92d5108023e368efec6aabc8a851a
Angel Bulnes <angelbulnes195@gmail.com>
2026-07-30 09:42:03 -0600

    Angel Bulnes Py Portfolio

---
+++ src/operators/logical-operators.py
@@ -0,0 +1,22 @@
+# and / comparison
+
+Result = True & True #return true
+Result2 = False & False #return false
+Result3 = True & False #return false
+Result = False & False #return false
+
+
+# or
+
+Result5 = True | False #return true
+Result6 = True | False #return true
+Result7 = True | False #return true
+Result = False | False #return false
+
+
+# not
+
+Result9 = not 2 == 3 #return false
+Result10 = not False #return true
+
+print(Result)
