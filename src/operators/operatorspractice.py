e09c3587c374ba79acdcfdf9815cc106cfeeca7f
Angel Bulnes <angelbulnes195@gmail.com>
2026-08-01 11:03:18 -0600

    Angel Bulnes Portfolio

---
+++ src/operators/operatorspractice.py
@@ -0,0 +1,88 @@
+# ARITHMETIC OPERATORS
+
+
+# addition: add two values
+add = 41 + 32
+print(add)
+
+# subtraction: subtract two values
+sub = 41 - 32
+print(sub)
+
+# multiplication: multiply two values
+mult = 41 * 32
+print(mult)
+
+# division: divide two values
+div = 41 / 32
+print(div)
+
+# exponentiation: raise a value to the power of another value
+exp = 41 ** 32
+print(exp)
+
+# low division: divide two values and round down to the nearest whole number
+low_div = 41 // 32
+print(low_div)
+
+# modulus: get the remainder of a division operation
+mod = 41 % 32
+print(mod)
+
+
+
+
+
+
+
+
+
+
+# COMPARISON OPERATORS
+
+is_equal_to = 41 == 32
+print(is_equal_to)
+
+is_diferent_from = 41 != 32
+print(is_diferent_from)
+
+is_greater_than = 41 > 32
+print(is_greater_than)
+
+is_less_than = 41 < 32
+print(is_less_than)
+
+is_greater_than_or_equal_to = 41 >= 32
+print(is_greater_than_or_equal_to)
+
+is_less_than_or_equal_to = 41 <= 32
+print(is_less_than_or_equal_to)
+
+
+
+
+
+# LOGICAL OPERATORS
+
+
+# and
+
+Result = True & True #return true
+Result2 = False & False #return false
+Result3 = False & False #return false
+
+
+# or
+
+Result4 = True | False #return true
+Result5 = False | False #return false
+
+
+# not
+
+Result6 = not 41 == 32 #return false
+Result7 = not False #return true
+
+print(Result)
+print(Result4)
+print(Result7)
